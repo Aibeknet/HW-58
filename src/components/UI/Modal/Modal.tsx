2ffd0c1a47b956964491f520b9788a0fe23120a1
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-07 17:34:41 +0600

    Edited the Model.tsx component and added CSS Transition.

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,32 +1,56 @@
+import React, { ReactNode } from 'react';
 import BackDrop from '../BackDrop/BackDrop.tsx';
+import { CSSTransition } from 'react-transition-group';
 
-interface Props extends React.PropsWithChildren {
+interface ButtonConfig {
+  type: 'primary' | 'danger';
+  label: string;
+  onClick: () => void;
+}
+
+interface Props {
   show: boolean;
   title?: string;
   closeModal?: () => void;
+  buttonConfigs?: ButtonConfig[];
+  children: ReactNode;
 }
 
-const Modal: React.FC<Props> = ({show, title = 'Modal title', closeModal, children}) => {
+const Modal: React.FC<Props> = ({ show, title = 'Modal title', closeModal, buttonConfigs, children }) => {
   return (
     <>
-      <BackDrop show={show} closeModal={closeModal}/>
-      <div className="modal show" style={{display: show ? 'block' : 'none'}}>
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5">{title}</h1>
-            </div>
-            <div className="p-2">
-              {children}
-            </div>
-            <div className="modal-footer">
-                <button onClick={closeModal} className="btn btn-primary">Close</button>
+      <BackDrop show={show} closeModal={closeModal} />
+      <CSSTransition in={show} timeout={300} classNames="fade" unmountOnExit>
+        <div className="modal">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5">{title}</h1>
+                <button
+                  onClick={closeModal}
+                  className="btn-close"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="p-2">
+                {children}
+              </div>
+              <div className="modal-footer">
+                {buttonConfigs?.map((btn, index) => (
+                  <button key={index} onClick={btn.onClick} className={`btn btn-${btn.type}`}>
+                    {btn.label}
+                  </button>
+                ))}
+                <button onClick={closeModal} className="btn btn-primary">Закрыть</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </CSSTransition>
     </>
   );
 };
 
 export default Modal;
+
+
